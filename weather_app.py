d9951483ca11eadacec26cd48b771da3e99798b4
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-16 21:06:24 +0000

    Add files via upload

--- weather_app.py
+++ weather_app.py
@@ -10,3 +10,16 @@ canvas.title('Weather app')
 f=("poppins", 15, "bold")
 t= ("poppins", 35, "bold")
 
+textfield = tk.Entry(canvas, font = t)
+textfield.pack(pady= 20)
+textfield.focus()
+
+
+label1 = tk.Label(canvas, font = t)
+label1.pack()
+
+label2= tk.Label(canvas, font= f)
+label2.pack()
+
+
+canvas.mainloop()
