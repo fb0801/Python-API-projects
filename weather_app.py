22d8f3bde503a979660f028fb5afa93d22cc65cf
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-16 21:01:28 +0000

    Add files via upload

---
+++ weather_app.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import requests
+import time
+
+
+canvas.tk.Tk()
+canvas.geomerty("600x500")
+canvas.title('Weather app')
+
+f=("poppins", 15, "bold")
+t= ("poppins", 35, "bold")
+
