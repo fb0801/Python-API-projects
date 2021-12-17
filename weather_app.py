4326bf21d6fff929e75717a12e3440bd28e0cd00
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-17 21:36:50 +0000

    Add files via upload

--- weather_app.py
+++ weather_app.py
@@ -1,11 +1,11 @@
 #modules to use
-import weather-config
+import weatherConfig
 import tkinter as tk
 import requests
 import time
 
 
-def getWeather():
+def getWeather(canvas):
     city = textfield.get()
     json_data= requests.get(api).json()
     condition = json_data['weather'][0]['main']
@@ -21,7 +21,8 @@ def getWeather():
 
     final_info = condition + '\n' + str(temp) + "°C"
     final_data = '\n' + "Max Temp: " + str(max_temp) + '\n' + 'Min Temp: ' + str(min_temp) + '\n' + 'Pressure: ' + str(pressure) + '\n' + 'Humidity: ' + str(humidity) + '\n' + "Wind speed: " + str(wind) + '\n' + "sunrise:" + sunrise + '\n' + "Sunset: " + sunset 
-    label1.config()
+    label1.config(text = final_info)
+    labal2.config(text = final_data)
     
 canvas.tk.Tk()
 canvas.geomerty("600x500")
@@ -33,7 +34,7 @@ t= ("poppins", 35, "bold")
 textfield = tk.Entry(canvas, font = t)
 textfield.pack(pady= 20)
 textfield.focus()
-
+textfield.bind('<Return>', getWeather)
 
 label1 = tk.Label(canvas, font = t)
 label1.pack()
