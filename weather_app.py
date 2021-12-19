bd45470fd54ef4ee720fb954c428e0b52b285a19
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-19 21:46:39 +0000

    Add files via upload

--- weather_app.py
+++ weather_app.py
@@ -7,7 +7,7 @@ import time
 
 def getWeather(canvas):
     city = textfield.get()
-   
+    
     json_data= requests.get(api).json()
     condition = json_data['weather'][0]['main']
     temp = int(json_data['main']['temp'] - 273.15)
@@ -16,14 +16,14 @@ def getWeather(canvas):
     pressure = json_data['main']['pressure']
     humidity = json_data['main']['humidity']
     wind = json_data['wind']['speed']
-    sunrise = time.strftime("%I%M%S", time.gmtime(json_data['sys']['sunrise']))
-    sunset = time.strftime("%I%M%S", time.gmtime(json_data['sys']['sunset']))
+    sunrise = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunrise'] -7200))
+    sunset = time.strftime("%I:%M:%S", time.gmtime(json_data['sys']['sunset'] -7200))
 
 
     final_info = condition + '\n' + str(temp) + "°C"
     final_data = '\n' + "Max Temp: " + str(max_temp) + '\n' + 'Min Temp: ' + str(min_temp) + '\n' + 'Pressure: ' + str(pressure) + '\n' + 'Humidity: ' + str(humidity) + '\n' + "Wind speed: " + str(wind) + '\n' + "sunrise:" + sunrise + '\n' + "Sunset: " + sunset 
     label1.config(text = final_info)
-    labal2.config(text = final_data)
+    label2.config(text = final_data)
     
 canvas=tk.Tk()
 canvas.geometry("600x500")
@@ -40,7 +40,7 @@ textfield.bind('<Return>', getWeather)
 label1 = tk.Label(canvas, font = t)
 label1.pack()
 
-label2 = tk.Label(canvas, font= f)
+label2 = tk.Label(canvas, font = f)
 label2.pack()
 
 
