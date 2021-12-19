70f2b59d5997350467b3ee4fe2e089e12cd3d7e2
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2021-12-19 21:19:28 +0000

    Add files via upload

--- weather_app.py
+++ weather_app.py
@@ -7,7 +7,7 @@ import time
 
 def getWeather(canvas):
     city = textfield.get()
-    
+   
     json_data= requests.get(api).json()
     condition = json_data['weather'][0]['main']
     temp = int(json_data['main']['temp'] - 273.15)
@@ -16,8 +16,8 @@ def getWeather(canvas):
     pressure = json_data['main']['pressure']
     humidity = json_data['main']['humidity']
     wind = json_data['wind']['speed']
-    sunrise = time.strfime("%I%M%S", time.gmtime(json_data['sys']['sunrise']))
-    sunset = time.strfime("%I%M%S", time.gmtime(json_data['sys']['sunset']))
+    sunrise = time.strftime("%I%M%S", time.gmtime(json_data['sys']['sunrise']))
+    sunset = time.strftime("%I%M%S", time.gmtime(json_data['sys']['sunset']))
 
 
     final_info = condition + '\n' + str(temp) + "°C"
@@ -40,7 +40,7 @@ textfield.bind('<Return>', getWeather)
 label1 = tk.Label(canvas, font = t)
 label1.pack()
 
-label2= tk.Label(canvas, font= f)
+label2 = tk.Label(canvas, font= f)
 label2.pack()
 
 
