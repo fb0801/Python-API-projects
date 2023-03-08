2adc41348cfc39635f758b5b30dfcdcb60d77775
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-08 19:16:58 +0000

    Add files via upload

--- fastApiProject/myapi.py
+++ fastApiProject/myapi.py
@@ -75,4 +75,11 @@ def update_student(student_id: int, student: UpdateStudent):
     
 
     #delete method 
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+        
+    del students[student_id]
+    return {"Message": "Student removed successfully"}
     
