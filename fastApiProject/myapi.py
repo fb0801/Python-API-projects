eb964f2a021a0810cd9d1d0d626bbc9defe2235c
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-08 15:54:30 +0000

    Add files via upload

--- fastApiProject/myapi.py
+++ fastApiProject/myapi.py
@@ -1,9 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+
+
 
 app = FastAPI()
+
+students = {
+    1:{
+    "name":"john",
+    "age": 17,
+    "class": "yr 18"
+    }
+}
 
 
 @app.get("/")
 
 def index():
     return {"name": "first data"}
+
+#path
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int =Path(None, description="The ID of student you want to view")):
+    return students[student_id]
+
+
+#query
+@app.get("/get-by-name")
+def get_student(name : str):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]    
+    return {"data: not found"}
