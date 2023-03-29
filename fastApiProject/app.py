090e6bc4a80fe2306cd9af79ae7413f6c69268d7
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-29 15:42:57 +0100

    Add files via upload

--- fastApiProject/app.py
+++ fastApiProject/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
-import sqlite3
+
+
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']   = 'sqlite:///data.db'
+db = SQLAlchemy(app)
 
 class Drink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
