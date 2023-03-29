83a0faddd36bdbeb0c2c1431e1f5f1caf92671cd
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-29 18:09:45 +0100

    Add files via upload

--- fastApiProject/app.py
+++ fastApiProject/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from requests import request
 
 
 
@@ -25,4 +26,33 @@ def index():
 
 @app.route('/drinks')
 def get_drinks():
-    return {'drinks': 'drink data'}
+    drinks = Drink.query.all()
+    output = []
+    for drink in drinks:
+        drink_data = {'name': drink.name, 'description':drink.description}
+        output.append(drink_data)
+
+    return {'drinks': output}
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    return ({'name': drink.name, 'description':drink.description})
+
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+    drink = Drink(name = request.json['name'], description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
+
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+def delete_drink():
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {'Error': 'not found'}
+    db.session.delete(drink)
+    db.session.commit()
+    return {'message': 'found'}
