5681c9fb0cad38840d0564542385c8195bd3860c
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-03-29 15:21:39 +0100

    Add files via upload

--- fastApiProject/stackapi.py
+++ fastApiProject/stackapi.py
@@ -3,4 +3,7 @@ import json
 
 response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
 
-print(response)
+for data in response.json()['items']:
+    print(data['title'])
+    print(data['link'])
+    print()
