4aa3f6497f00b37f74157f9f4943aa88f7ea32fc
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-04-01 20:09:51 +0100

    Add files via upload

--- fastApiProject/app2.py
+++ fastApiProject/app2.py
@@ -1,17 +1,31 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, Model
 import requests
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
+app.config["SQLAlCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
 
-videos ={}
+class VideoModel(db.Model):
+    id = db.column(db.integer, primary_key=True)
+    name  = db.column(db.string(100), nullable = False)
+    views = db.column(db.integer, nullable = False)
+
+    def __repr__(self):
+        return f"Video(name={name}, views={views}, likes = {likes})"
+
+
+db.create_all()
+
+
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument('name', type=str, help="video name", required=True)
 video_put_args.add_argument('views', type=int, help="video views", required=True)
 video_put_args.add_argument('views', type=int, help="video views", required=True)
 video_put_args.add_argument('like', type=int, help="video likes", required=True)
+videos ={}
 
 def abort_video(video_id):
     if video_id not in videos:
@@ -23,7 +37,7 @@ def abort_video_if_exist(video_id):
 
 class Video(Resource):
     def get(self, video_id):
-        return {"Hello world"}
+        abort_video(video_id)
     abort_video()
     
     def put(self,video_id):
