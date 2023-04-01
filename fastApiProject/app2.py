4371f5dcd5a1fcc727d5f84e65202fbc01208193
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-04-01 19:08:53 +0100

    Add files via upload

--- fastApiProject/app2.py
+++ fastApiProject/app2.py
@@ -3,3 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from requests import request
 from flask_restful import Api, Resource
 
+app = Flask(__name__)
+api = Api(app)
+
+class HellowWorld(Resource):
+    def get(self, name, test):
+        return {"Hello world"}
+    
+
+api.add_resource(HellowWorld,"/")
+
+if __name__ =="main":
+    app.run(debug=True)
