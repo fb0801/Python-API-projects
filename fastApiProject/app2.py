77bac33ef12098cc08d042ad2899111a93de11d6
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-04-01 19:24:24 +0100

    Add files via upload

--- fastApiProject/app2.py
+++ fastApiProject/app2.py
@@ -1,17 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from requests import request
-from flask_restful import Api, Resource
+import requests
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class HellowWorld(Resource):
-    def get(self, name, test):
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument('name', type=str, help="video name", required=True)
+video_put_args.add_argument('views', type=int, help="video views", required=True)
+video_put_args.add_argument('views', type=int, help="video views", required=True)
+video_put_args.add_argument('like', type=int, help="video likes", required=True)
+
+
+
+class Video(Resource):
+    def get(self, video_id):
         return {"Hello world"}
     
+    def put(self,video_id):
+        return{}
+    
 
-api.add_resource(HellowWorld,"/")
+api.add_resource(Video,"/video/<int:video_id")
 
 if __name__ =="main":
     app.run(debug=True)
